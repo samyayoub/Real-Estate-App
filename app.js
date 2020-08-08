9f9d905aa329d784ef15409304270e798d508ff5
Samy Ayoub <samyayoub@gmail.com>
2020-08-08 02:00:02 -0500

    Added functionality to calculate total interest will be paid

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
 // Calling function to calculate results
-let calculateResult = document.querySelector("#calculateResult");
+var calculateMonthlyPayment = document.querySelector(
+	"#calculateMonthlyPayment"
+);
+var calculateInterestPaid = document.querySelector("#calculateInterestPaid");
 
-function calculations() {
+function monthlyPaymentCalculations() {
 	// Grabbing values entered by end user
-	let loanValue = document.querySelector("#loanValue").value;
-	let loanTerm = document.querySelector("#loanTerm").value;
-	let loanInterest = document.querySelector("#loanInterest").value;
-	let interest = loanInterest / 100;
+	var loanValue = document.querySelector("#loanValue").value;
+	var loanTerm = document.querySelector("#loanTerm").value;
+	var loanInterest = document.querySelector("#loanInterest").value;
+	var interest = loanInterest / 100;
 	interest = interest / 12;
 
 	// Math to calculate monthly payment
 	// D = {[(1 + r)^n] - 1} / [r(1 + r)^n]
-	let noOfMonths = 12 * loanTerm;
-	let temp1 = Math.pow(1 + interest, noOfMonths);
-	let temp2 = temp1 - 1;
-	let temp3 = interest * temp1;
-	let temp4 = temp2 / temp3;
-	let result = loanValue / temp4;
-	result = result.toFixed(2);
+	var noOfMonths = 12 * loanTerm;
+	var temp1 = Math.pow(1 + interest, noOfMonths);
+	var temp2 = temp1 - 1;
+	var temp3 = interest * temp1;
+	var temp4 = temp2 / temp3;
+	var result = loanValue / temp4;
+	result = result
+		.toFixed(2)
+		.toString()
+		.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Puts comma for thousands
 
 	document.querySelector(
 		"#result"
 	).innerHTML = `Monthly mortgage payment = $${result}`;
 }
 
+// function calculations() {
+// 	monthlyPaymentCalculations();
+// 	interestCalculations();
+// }
+
+function interestCalculations() {
+	var loanValue = document.querySelector("#loanValue").value;
+	var loanTerm = document.querySelector("#loanTerm").value;
+	var loanInterest = document.querySelector("#loanInterest").value;
+	var interest = loanInterest / 100;
+	let interestTotal = loanValue * interest * loanTerm;
+	interestTotal = interestTotal
+		.toFixed(2)
+		.toString()
+		.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Puts comma for thousands
+
+	document.querySelector(
+		"#result"
+	).innerHTML = `Total Interest will be paid = ${interestTotal}`;
+}
+
 // Adding event listener to wait on end user to press "Calculate" button
-calculateResult.addEventListener("click", calculations);
+calculateMonthlyPayment.addEventListener("click", monthlyPaymentCalculations);
+calculateInterestPaid.addEventListener("click", interestCalculations);
